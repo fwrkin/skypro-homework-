001335dd2bd162e94d34be8f34de3aaf7ba94061
Kira Fedotova <k.fedotova@it-park.tech>
2024-04-15 22:22:29 +0300

    widget function

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
-from src.masks import mask_card_number, mask_account_number
+from src.masks import mask_account_number, mask_card_number
+
 
 def mask_card_or_account_number(data: str) -> str:
     """Принимает на вход строку с информацией — тип карты/счета и номер карты/счета.
@@ -6,9 +7,15 @@ def mask_card_or_account_number(data: str) -> str:
     if "Счет" in data:
         return f"Счет {mask_account_number(data)}"
     else:
-        card_data_with_number = data.split(' ')
+        card_data_with_number = data.split(" ")
         card_number = "".join(card_data_with_number[-1])
         if len(card_data_with_number) == 3:
             return f"{' '.join(card_data_with_number[:-1])} {mask_card_number(card_number)}"
         return f"{card_data_with_number[0]} {mask_card_number(card_number)}"
 
+
+def get_new_type_of_date(date: str) -> str:
+    """возвращает строку с датой в нужном нам виде"""
+    type_of_date = date.split("T")
+    date_ = type_of_date[0].split("-")
+    return f"{date_[2]}.{date_[1]}.{date_[0]}"
