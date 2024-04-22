38c2b312aceb10d54dabf42222597680716c12d6
Kira Fedotova <k.fedotova@it-park.tech>
2024-04-22 17:40:32 +0300

    new functions with sorted data and changed data

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+def change_data(data: list[dict], key_meaning: str = 'EXECUTED') -> list[dict]:
+    """
+    принимает на вход список словарей и значение для ключа state и возвращает новый список,
+    содержащий только те словари, у которых ключ state содержит переданное в функцию значение
+    """
+    updated_list = []
+    for item in data:
+        if item['state'] == key_meaning:
+            updated_list.append(item)
+    return updated_list
+
+
+def sorted_by_data(data: list[dict], reversed: bool = True) -> list[dict]:
+    """
+    принимает на вход список словарей и возвращает новый список,
+    в котором исходные словари отсортированы по убыванию даты
+    """
+    new_data = sorted(data, key=lambda x: x['data'], reverse=reversed)
+    return new_data
