ab6d1bf7fca888629e854011ed91fc63bf78550a
Kira Fedotova <k.fedotova@it-park.tech>
2024-06-07 17:37:14 +0300

    csv and xlsx

--- tests/test_reading_csv_and_xlsx_files.py
+++ tests/test_reading_csv_and_xlsx_files.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock, patch
 
 from pandas import DataFrame
 
-from src.readingCSVandXLSXfiles import reading_csv_files, reading_xlsx_files
+from src.reading_csv_and_xlsx_files import reading_csv_files, reading_xlsx_files
 
 
 @patch("pandas.read_csv")
